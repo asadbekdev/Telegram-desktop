648099f25c0398858fc9df50163f3e75816863c1
Asadbek <67022917+Asadbek77@users.noreply.github.com>
2021-02-21 12:25:47 -0500

    Update block.js

--- js/block.js
+++ js/block.js
@@ -5,19 +5,19 @@ document.addEventListener("contextmenu", (event) =>
 );
 document.onkeydown = function (e) {
   if (event.keyCode == 123) {
-    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    swal("Oops", "Bloklangan Amaliyot!","😡", { icon: "warning" });
     return false;
   }
   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    swal("Oops", "Bloklangan Amaliyot!", "😡",{ icon: "warning" });
     return false;
   }
   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    swal("Oops", "Bloklangan Amaliyot!", "😡",{ icon: "warning" });
     return false;
   }
   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    swal("Oops", "Bloklangan Amaliyot!", "😡",{ icon: "warning" });
     return false;
   }
 };
