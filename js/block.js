ea8375578290eb13cb7b738b8f3eebcdbf741960
Asadbek <67022917+Asadbek77@users.noreply.github.com>
2021-02-21 12:22:57 -0500

    Add files via upload

---
+++ js/block.js
@@ -0,0 +1,23 @@
+document.addEventListener("contextmenu", (event) =>
+  event.preventDefault(
+    swal("Oops", "Bloklangan Amaliyot!", { icon: "warning" })
+  )
+);
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    swal("Oops", "Bloklangan Amaliyot!", "Saytimni Codeni olishga urinmang","😡" ,{ icon: "warning" });
+    return false;
+  }
+};
